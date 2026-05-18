b26432d9959abd0488b84dff5adc92cda81cdca8
rnktlhr <rinakitluhur10@gmail.com>
2026-05-18 10:50:01 +0700

    menambahkan tabel users

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,24 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
 }
