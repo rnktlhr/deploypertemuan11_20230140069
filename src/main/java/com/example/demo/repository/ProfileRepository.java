b31cfc2985cbdd304f46004349da2f3cc2f974c4
rnktlhr <rinakitluhur10@gmail.com>
2026-05-18 10:53:37 +0700

    menambahkan code profile repository

--- src/main/java/com/example/demo/repository/ProfileRepository.java
+++ src/main/java/com/example/demo/repository/ProfileRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public interface ProfileRepository {
+import com.example.demo.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, String> {
 }
