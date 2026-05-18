dee2cf643ad6d58e7f4400f70cfbafd799afa9b0
rnktlhr <rinakitluhur10@gmail.com>
2026-05-18 10:58:39 +0700

    menambahkan code RegisterRequest

--- src/main/java/com/example/demo/model/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/model/dto/RegisterRequest.java
@@ -1,4 +1,12 @@
 package com.example.demo.model.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String username;
+    private String password;
+
+    private String nama;
+    private String alamat;
 }
