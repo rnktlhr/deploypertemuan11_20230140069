ae064e612a371e03449625d4200cd39b4133507e
rnktlhr <rinakitluhur10@gmail.com>
2026-05-18 10:46:11 +0700

    menambahkan tabel profile

--- src/main/java/com/example/demo/model/Profile.java
+++ src/main/java/com/example/demo/model/Profile.java
@@ -1,4 +1,24 @@
 package com.example.demo.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
+    @Id
+    private String id;
+    private String nama;
+    private String alamat;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
